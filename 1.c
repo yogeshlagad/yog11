800cde3fb2d958d54c128cf8398f82178e2ccec5
yogeshlagad <101618435+yogeshlagad@users.noreply.github.com>
2022-03-16 14:12:58 +0530

    clang

---
+++ 1.c
@@ -0,0 +1,93 @@
+
+
+/*
+#include<stdio.h>
+
+int main(void)
+{
+printf("\n HAR HAR MAHADEV!!!");
+printf("\n c-dac only!!!");
+    return 0;
+}
+
+
+#include<stdio.h>
+
+int main()
+{
+
+int num1=2,num2=2,num3=5;
+
+printf("\n value of num1=%d num2=%d num3=%d",num3,num2,num1);
+
+   return 0; 
+}
+
+#include<stdio.h>
+int main(void)
+{
+    int i=123;
+    float f=21.12;
+    char ch='A';
+     
+     printf("\n%d \n%c \n%f",i,ch,f);
+
+    return 0;
+}
+
+
+
+#include <stdio.h>
+
+int main(void)
+{
+    int n1=50;  // variable initialization 
+    int val=40;
+    int res=35;
+
+    printf("%d %d %d",n1,val,res);
+     printf("\n %d , %d , %d",n1,val,res);
+      printf("\n %d %d %d",val,res,n1);
+       printf("\n n1,val,res");
+       printf("\n First = %d Second  = %d Third= %d",n1,val,res);
+
+    return 0;
+}
+
+#include<stdio.h>
+int main()
+{
+    float fval=6.87;
+    printf("Fval = %f",fval);
+    printf("\n size of float = %d ",sizeof(fval));
+     printf("\n size of float datatype = %d ",sizeof(float));
+     printf("\n size of dobule datatype = %d",sizeof(double));
+     printf("\n size of dobule datatype = %d",sizeof(char));
+
+    
+
+    return 0;
+}*/
+
+
+
+#include<stdio.h>
+int main(void)
+{
+    int num=35; // variable intialization
+    //printf("Num");
+    printf("\n Num = %d",num);
+     // Num = 35 
+    printf("\n size = %d ",sizeof(num));
+    printf("\n size of integer datatype = %d",sizeof(int));
+     printf("\n size of any whole number = %d",sizeof(12345));
+
+    return 0;
+}
+
+
+
+
+
+
+
